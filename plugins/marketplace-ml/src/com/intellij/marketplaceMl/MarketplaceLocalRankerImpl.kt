6bd7652c493f61116449e3034ee37b71ecb287b9
yyyair <glu0n@gmail.com>
2025-03-14 19:24:14 +0200

    Remvoe marketplace

--- plugins/marketplace-ml/src/com/intellij/marketplaceMl/MarketplaceLocalRankerImpl.kt
+++
@@ -1,5 +0,0 @@
-package com.intellij.marketplaceMl
-
-import com.intellij.ide.plugins.marketplace.ranking.MarketplaceLocalRanker
-
-private class MarketplaceLocalRankerImpl : MarketplaceLocalRanker by MarketplaceLocalRankingService.getInstance()
